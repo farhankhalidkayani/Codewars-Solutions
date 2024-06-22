3b6fcc5d5d46132775d0da48ec0a3c397cd5cee0
Farhan Khalid <rj.farhan4232@gmail.com>
2024-06-22 12:04:48 +0500

    Completed Invert Values

---
+++ 8kyu/HelloWorldFunction.py
@@ -0,0 +1,5 @@
+#DESCRIPTION:
+#Make a simple function called greet that returns the most-famous "hello world!".
+
+def greet():
+   return "hello world!"
---
+++ 8kyu/Reversed-Strings.py
@@ -0,0 +1,8 @@
+#DESCRIPTION:
+#Complete the solution so that it reverses the string passed into it.
+
+#'world'  =>  'dlrow'
+#'word'   =>  'drow'
+
+def solution(string):
+    return string[::-1]
