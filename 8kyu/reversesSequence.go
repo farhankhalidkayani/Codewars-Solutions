56a4064f526f5ede291c6752c622d7af92710939
Farhan Khalid <rj.farhan4232@gmail.com>
2024-07-04 19:16:49 +0500

    Solved Some Kata in GO

---
+++ 8kyu/reversesSequence.go
@@ -0,0 +1,13 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+package kata
+
+func ReverseSeq(n int) []int {
+	res := []int{}
+	for i := n; i >= 1; i-- {
+		res = append(res, i)
+	}
+	return res
+}
